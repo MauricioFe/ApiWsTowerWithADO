d1ee3c9ee9a1c2e9e8818735f3fbe83820671c8d
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-01 15:07:25 -0300

    Correções de erros

--- ApiWsTower/ApiWsTower/Models/Relatos.cs
+++ ApiWsTower/ApiWsTower/Models/Relatos.cs
@@ -9,10 +9,10 @@ namespace ApiWsTower.Models
     public class Relatos
     {
         public int ID { get; set; }
-        public string  Relato { get; set; }
-        public string  Imagem { get; set; }
-        public Decimal  Latitude{ get; set; }
-        public Decimal Longitude { get; set; }
+        public string Relato { get; set; }
+        public string Imagem { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
         public Nullable<int> UsuarioId { get; set; }
     }
 }
