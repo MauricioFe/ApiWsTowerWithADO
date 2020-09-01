81a1a63be963acc48c66790c5af6f229f247e7b2
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-01 15:06:32 -0300

    Criação das models

---
+++ ApiWsTower/ApiWsTower/Models/Relatos.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiWsTower.Models
+{
+    public class Relatos
+    {
+        public int ID { get; set; }
+        public string  Relato { get; set; }
+        public string  Imagem { get; set; }
+        public Decimal  Latitude{ get; set; }
+        public Decimal Longitude { get; set; }
+        public Nullable<int> UsuarioId { get; set; }
+    }
+}
